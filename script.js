e6136b71060a5b6bce58eb69e11bfe9d38f4aabb
vivaeur <adam.micro2008@gmail.com>
2024-11-30 19:36:20 +0100

    a

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-let editor = document.querySelector("textarea");
-let button = document.querySelector("button");
+var btncopy = document.querySelector('.js-copy');
+if(btncopy) {
+    btncopy.addEventListener('click', docopy);
+}
 
+function docopy() {
+    var range = document.createRange();
+    var target = this.dataset.target;
+    var fromElement = document.querySelector(target);
+    var selection = window.getSelection();
 
-utton.addEventListener('click', () => {
-    editor.select();
-    // document.execCommand("copy"); méthode numéro 1
-    navigator.clipboard.writeText(editor.value); // méthode numéro 2
-    button.innerText = "Copié !";
-  });
+    range.selectNode(fromElement);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+        var result = document.execCommand('copy');
+        if (result) {
+            // La copie a réussi
+            alert('Copié !');
+        }
+    }
+    catch(err) {
+        // Une erreur est surevnue lors de la tentative de copie
+        alert(err);
+    }
+
+    selection = window.getSelection();
+
+    if (typeof selection.removeRange === 'function') {
+        selection.removeRange(range);
+    } else if (typeof selection.removeAllRanges === 'function') {
+        selection.removeAllRanges();
+    }
+}
