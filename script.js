eb0a6d0123ca69a0e203c9b0dcfc27cb5997d0e8
vivaeur <adam.micro2008@gmail.com>
2024-11-30 22:09:22 +0100

    update

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ function docopy() {
         selection.removeAllRanges();
     }
 }
+
+
+
+const a = document.querySelector('.clock');
+
+
+
+
+
+
+const timer = () =>{
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+   // console.log(hours,":",minutes,":",seconds);
+
+
+   const template = `<span> Heure :  </span>
+                     <span>${hours} </span>  
+                     <span>${minutes} </span> 
+                     <span>${seconds}</span>
+   
+   `
+
+
+   a.innerHTML =template;
+};
+
+
+
+setInterval(timer,1000);
