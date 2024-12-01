b90c1fdca60631c93800799346dfb65d2a196cd5
vivaeur <adam.micro2008@gmail.com>
2024-12-01 01:20:55 +0100

    update

--- script.js
+++ script.js
@@ -50,8 +50,8 @@ const timer = () =>{
 
 
    const template = `<span> Heure :  </span>
-                     <span>${hours} </span>  
-                     <span>${minutes} </span> 
+                     <span>${hours}. </span>  
+                     <span>${minutes}. </span> 
                      <span>${seconds}</span>
    
    `
@@ -63,3 +63,20 @@ const timer = () =>{
 
 
 setInterval(timer,1000);
+
+
+
+$(document).ready(function(){
+    var mouseX, mouseY;
+    var ww = $( window ).width();
+    var wh = $( window ).height();
+    var traX, traY;
+    $(document).mousemove(function(e){
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      traX = ((4 * mouseX) / 570) + 40;
+      traY = ((4 * mouseY) / 570) + 50;
+      console.log(traX);
+      $(".title").css({"background-position": traX + "%" + traY + "%"});
+    });
+  });
